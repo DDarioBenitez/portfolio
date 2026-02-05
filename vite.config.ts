6305a783ecd4c6692c591b731e70645cb1e6c44f
Dario Benitez <reedq2.alt@gmail.com>
2026-02-05 15:14:33 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import glsl from "vite-plugin-glsl";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), glsl()],
+    plugins: [react(), tailwindcss()],
     optimizeDeps: {
-        include: ['framer-motion', 'three']
+        include: ["framer-motion", "three"],
     },
     resolve: {
         alias: {
-            '@': '/src'
-        }
-    }
+            "@": "/src",
+        },
+    },
 });
